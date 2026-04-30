63daea824db5a2a2a6d2b777b2f4bdb186ece718
Krzysztof Kluczek <kk@plastic-demo.pl>
2026-05-01 00:27:00 +0200

    vga_zuzel_09_laps_scorebars

--- src/project.v
+++ src/project.v
@@ -74,15 +74,15 @@ module tt_um_KK_VGA01(
 
   // RESET_Y min/max = 295..456
   wire sp1on, sp2on, sp3on, sp4on;
-  motor_core motor1( .RESET_Y(10'd322), .ctrl(mt_ctrl), .clk(clk), .steer(steer[0]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .spron(sp1on) );
-  motor_core motor2( .RESET_Y(10'd355), .ctrl(mt_ctrl), .clk(clk), .steer(steer[1]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .spron(sp2on) );
-  motor_core motor3( .RESET_Y(10'd388), .ctrl(mt_ctrl), .clk(clk), .steer(steer[2]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .spron(sp3on) );
-  motor_core motor4( .RESET_Y(10'd421), .ctrl(mt_ctrl), .clk(clk), .steer(steer[3]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .spron(sp4on) );
+  wire[2:0] laps1;
+  wire[2:0] laps2;
+  wire[2:0] laps3;
+  wire[2:0] laps4;
+  motor_core motor1( .RESET_Y(10'd322), .ctrl(mt_ctrl), .clk(clk), .steer(steer[0]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .goal_in(goalmsk), .spron(sp1on), .laps(laps1) );
+  motor_core motor2( .RESET_Y(10'd355), .ctrl(mt_ctrl), .clk(clk), .steer(steer[1]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .goal_in(goalmsk), .spron(sp2on), .laps(laps2) );
+  motor_core motor3( .RESET_Y(10'd388), .ctrl(mt_ctrl), .clk(clk), .steer(steer[2]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .goal_in(goalmsk), .spron(sp3on), .laps(laps3) );
+  motor_core motor4( .RESET_Y(10'd421), .ctrl(mt_ctrl), .clk(clk), .steer(steer[3]), .hpos(pix_x), .vpos(pix_y), .hsync(hsync), .track_in(trkon), .goal_in(goalmsk), .spron(sp4on), .laps(laps4) );
 
-  wire mR = sp1on | sp4on;
-  wire mG1 = sp2on | sp3on | sp4on;
-  wire mG0 = sp2on | sp4on;
-  wire mB = sp3on;
   // 1001xxxxx
   // 1010
   // 10011xxxx
@@ -90,9 +90,22 @@ module tt_um_KK_VGA01(
   //wire goalmsk = pix_y[8] & ~trkon & pix_x[8] & ~pix_x[7] & (pix_x[5] ^ pix_x[6]);
   wire goalmsk = pix_y[8] & ~trkon & pix_x[8] & ~pix_x[7] & (pix_x[6] ^ pix_x[5]) & (pix_x[6] ^ pix_x[4]);
   wire goal = goalmsk & (pix_x[3] ^ pix_y[3]);
-  assign R = video_active ? {mR, mR|goal} : 2'b00;
-  assign G = video_active ? {mG1, mG0|trkon|goal} : 2'b00;
-  assign B = video_active ? {mB, mB|goal} : 2'b00;
+  wire[3:0] score_x = pix_x[7:4] ^ 4'b1000;
+  wire[2:0] score_y = pix_y[6:4];
+  wire scoremask = score_x[3] & pix_x[8] & score_y[2] & pix_y[7] & ~pix_y[8];
+
+  wire p1 = sp1on | (scoremask & (score_y[1:0]==0) & (score_x[2:0]<laps1));
+  wire p2 = sp2on | (scoremask & (score_y[1:0]==1) & (score_x[2:0]<laps2));
+  wire p3 = sp3on | (scoremask & (score_y[1:0]==2) & (score_x[2:0]<laps3));// | (goal & mt_ctrl[14]);
+  wire p4 = sp4on | (scoremask & (score_y[1:0]==3) & (score_x[2:0]<laps4));
+  wire p0 = goal & mt_ctrl[14];
+  wire mR = p1 | p4;
+  wire mG1 = p2 | p3 | p4;
+  wire mG0 = p2 | p4;
+  wire mB = p3;
+  assign R = video_active ? {mR, mR|p0} : 2'b00;
+  assign G = video_active ? {mG1, mG0|(trkon&~scoremask)|p0} : 2'b00;
+  assign B = video_active ? {mB, mB|p0} : 2'b00;
   
   // Suppress unused signals warning
   //wire _unused_ok_ = &{moving_x, pix_y};
